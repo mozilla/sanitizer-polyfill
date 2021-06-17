6a661717e73c0c49af69ae76259e1e8c7b91405a
Frederik Braun <fbraun@mozilla.com>
2021-06-17 11:21:05 +0200

    first steps of polyfilling but lacking all sanitizing-logic and algorithms

---
+++ src/polyfill.js
@@ -0,0 +1,64 @@
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import {_sanitizeDocFragment, _sanitizeDocument, _fragmentParser, _normalizeConfig} from 'sanitizer.js';
+
+function setup() {
+  // name of our global object
+  const GLOBALNAME = "Sanitizer";
+
+  // name of the innerHTML-setter,
+  // https://github.com/WICG/sanitizer-api/issues/100
+  // when changing this, also change the function declaration below manually.
+  const SETTER_NAME = "setSanitizedHTML";
+
+  if (typeof window === "undefined") {
+    return;
+  }
+  if (window.isSecureContext) {
+    // don't polyfill if already defined
+    if (typeof window[GLOBALNAME] === "function") {
+        return;
+    }
+    //
+    const sanitizer = function(config) {
+      const api = Object.create({});
+
+      let normalizedConfig = _normalizeConfig(config);
+      Object.assign(api, {
+        sanitize(input) {
+          if (input instanceof DocumentFragment) {
+            return _sanitizeDocFragment(config, input);
+          }
+          if (input instanceof Document) {
+            return _sanitizeDocument(config, input)
+          }
+          return new TypeError("Can't Sanitize input of type " + input.prototype);
+        },
+        sanitizeFor(localName, input) {
+        // TODO: should parse/sanitize/filter/validate values for localName.
+          const context = document.createElement(localName);
+          let fragment = _fragmentParser(context, input);
+          const sanitizedFragment = _sanitizeDocFragment(fragment);
+          context.append(sanitizedFragment);
+          return context;
+        },
+        config: normalizedConfig,
+      });
+      return Object.freeze(api);
+    }
+    window[GLOBALNAME] = sanitizer;
+    Element.prototype[SETTER_NAME] = function setSanitizedHTML(input, sanitizerObj) {
+      if (!(sanitizerObj instanceof window[GLOBALNAME])) {
+        return new TypeError("sanitizerObj is not of type "+GLOBALNAME);
+      }
+      const fragment = _fragmentParser(this, input);
+      const sanitizedFragment = _sanitizeDocFragment(fragment);
+      this.append(sanitizedFragment);
+    };
+  }
+}
+
+setup();
---
+++ src/sanitizer.js
@@ -0,0 +1,28 @@
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ 
+// base algorithms for a sanitizer
+
+// eslint-disable-next-line no-unused-vars
+export const _sanitizeDocFragment = function _sanitizeDocFragment(config, input) {
+    // TODO implement this function and remove eslint-disable comment above
+};
+
+// eslint-disable-next-line no-unused-vars
+export const _sanitizeDocument = function _sanitizeDocument(config, input) {
+    // TODO implement this function and remove eslint-disable comment above
+};
+
+// a parser that doesn't run scripts or loads resources
+// eslint-disable-next-line no-unused-vars
+export const _fragmentParser = function _fragmentParser(ctx, input) {
+    // note: ctx is of type Node (element?)
+    // TODO implement this function and remove eslint-disable comment above
+};
+
+// eslint-disable-next-line no-unused-vars
+export const _normalizeConfig = function _normalizeConfig(config) {
+    // TODO implement this function and remove eslint-disable comment above
+}
