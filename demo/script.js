2f202fab8b1186d0bbd5e512899c940b3029da04
Rohan <rohan.kumarjha.cd.mec19@itbhu.ac.in>
2021-08-04 14:44:49 +0530

    demo

---
+++ demo/script.js
@@ -0,0 +1,65 @@
+const inputEl = document.getElementById("input");
+const toutEl = document.getElementById("textoutput");
+const houtEl = document.getElementById("htmloutput");
+  
+  
+  addEventListener("load", function() {
+    inputEl.onkeyup = doSanitize;
+    // smoke test
+    try { console.log("typeof Sanitizer: " + (typeof Sanitizer)); } catch(e) { };
+    try { window.s = new Sanitizer(); } catch(e) { console.warn('Sanitizer is not a constructor:', e);  };
+    
+  
+  });
+  
+  function fillExample() {
+    inputEl.value = `<p>hi! <svg/onload="alert(1)" />well <em>please</em> <a href="javascript:alert(2)" title="pretty-please">click me!</a>`;
+  };
+  
+  function prepareInput() {
+    switch (inputSelect.value) {
+      case "String":
+        return input.value;
+        break;
+      case "DocumentFragment":
+        const template = document.createElement("template");
+        template.innerHTML = input.value;
+        return template.content;
+        break;
+      case "Document":
+        const parser = new DOMParser();
+        return parser.parseFromString(input.value, "text/html");      
+        break;
+      default:
+      throw new Error("invalid value for input type select");
+    }
+  }
+    
+  function doSanitize() {
+
+    const inputValue = prepareInput();
+    
+    houtEl.setHTML(inputValue,{});
+    
+    let docFragment = houtEl.innerHTML;
+    
+    toutEl.innerHTML="";
+    toutEl.append(docFragment)
+    
+  }
+  
+  
+//   function disableUnsupported() {
+//     function disableEl(el) {
+//       const warnText = "Sanitizer is not available. Please switch to a browser that defines `Sanitizer`, e.g. Firefox Nightly with `dom.security.sanitizer.enabled` set to true.";    
+//       el.disabled = true;
+//       el.classList.add("disabled");
+//       el.title = warnText;
+//       if ("placeholder" in el) { el.placehoder = warnText; }
+//     }
+    // disableEl(inputEl);
+    
+    
+    inputEl.onkeyup = () => {};
+  
+  
