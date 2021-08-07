b96ae591bb2e58d71e24af70463b6a3fa4016bf1
Edward <14011954+0xedward@users.noreply.github.com>
2021-08-07 13:12:14 -0400

    Fix ESLint lint errors

--- demo/script.js
+++ demo/script.js
@@ -2,44 +2,57 @@ const inputEl = document.getElementById("input");
 const toutEl = document.getElementById("textoutput");
 const houtEl = document.getElementById("htmloutput");
 
-  addEventListener("load", function() {
-    inputEl.onkeyup = doSanitize;
-    // smoke test
-    try { console.log("typeof Sanitizer: " + (typeof Sanitizer)); } catch(e) { };
-    try { window.s = new Sanitizer(); } catch(e) { console.warn('Sanitizer is not a constructor:', e);};
-  });
-  
-  function fillExample() {
-    inputEl.value = `<p>hi! <svg/onload="alert(1)" />well <em>please</em> <a href="javascript:alert(2)" title="pretty-please">click me!</a>`;
-  };
-  
-  function prepareInput() {
-    switch (inputSelect.value) {
-      case "String":
-        return input.value;
-        break;
-      case "DocumentFragment":
-        const template = document.createElement("template");
-        template.innerHTML = input.value;
-        return template.content;
-        break;
-      case "Document":
-        const parser = new DOMParser();
-        return parser.parseFromString(input.value, "text/html");      
-        break;
-      default:
+addEventListener("load", function () {
+  inputEl.onkeyup = doSanitize;
+  // smoke test
+  try {
+    console.log("typeof Sanitizer: " + typeof Sanitizer);
+  } catch (e) {}
+  try {
+    window.s = new Sanitizer();
+  } catch (e) {
+    console.warn("Sanitizer is not a constructor:", e);
+  }
+});
+
+/**
+ *
+ */
+function fillExample() {
+  inputEl.value = `<p>hi! <svg/onload="alert(1)" />well <em>please</em> <a href="javascript:alert(2)" title="pretty-please">click me!</a>`;
+}
+
+/**
+ *
+ */
+function prepareInput() {
+  switch (inputSelect.value) {
+    case "String":
+      return input.value;
+      break;
+    case "DocumentFragment":
+      const template = document.createElement("template");
+      template.innerHTML = input.value;
+      return template.content;
+      break;
+    case "Document":
+      const parser = new DOMParser();
+      return parser.parseFromString(input.value, "text/html");
+      break;
+    default:
       throw new Error("invalid value for input type select");
-    }
   }
-    
-  function doSanitize() {
-    const inputValue = prepareInput();
-    houtEl.setHTML(inputValue,{});
-    let docFragment = houtEl.innerHTML;
-    toutEl.innerHTML="";
-    toutEl.append(docFragment)
-  }
- 
-  inputEl.onkeyup = () => {};
-  
-  
+}
+
+/**
+ *
+ */
+function doSanitize() {
+  const inputValue = prepareInput();
+  houtEl.setHTML(inputValue, {});
+  let docFragment = houtEl.innerHTML;
+  toutEl.innerHTML = "";
+  toutEl.append(docFragment);
+}
+
+inputEl.onkeyup = () => {};
