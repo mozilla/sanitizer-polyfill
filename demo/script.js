66b44c85a872082432c20fb232dbea67619899cc
Rohan <rohan.kumarjha.cd.mec19@itbhu.ac.in>
2021-08-23 14:09:28 +0200

    Demo: Further UI improvements

--- demo/script.js
+++ demo/script.js
@@ -1,3 +1,4 @@
+/* globals Sanitizer */
 const inputEl = document.getElementById("input");
 const toutEl = document.getElementById("textoutput");
 const houtEl = document.getElementById("htmloutput");
@@ -5,9 +6,7 @@ const houtEl = document.getElementById("htmloutput");
 addEventListener("load", function () {
   inputEl.onkeyup = doSanitize;
   // smoke test
-  try {
-    console.log("typeof Sanitizer: " + typeof Sanitizer);
-  } catch (e) {}
+  console.log("typeof Sanitizer: " + typeof Sanitizer);
   try {
     window.s = new Sanitizer();
   } catch (e) {
@@ -18,6 +17,7 @@ addEventListener("load", function () {
 /**
  *
  */
+// eslint-disable-next-line no-unused-vars
 function fillExample() {
   inputEl.value = `<p>hi! <svg/onload="alert(1)" />well <em>please</em> <a href="javascript:alert(2)" title="pretty-please">click me!</a>`;
 }
@@ -25,34 +25,12 @@ function fillExample() {
 /**
  *
  */
-function prepareInput() {
-  switch (inputSelect.value) {
-    case "String":
-      return input.value;
-      break;
-    case "DocumentFragment":
-      const template = document.createElement("template");
-      template.innerHTML = input.value;
-      return template.content;
-      break;
-    case "Document":
-      const parser = new DOMParser();
-      return parser.parseFromString(input.value, "text/html");
-      break;
-    default:
-      throw new Error("invalid value for input type select");
-  }
-}
-
-/**
- *
- */
 function doSanitize() {
-  const inputValue = prepareInput();
-  houtEl.setHTML(inputValue, {});
-  let docFragment = houtEl.innerHTML;
-  toutEl.innerHTML = "";
-  toutEl.append(docFragment);
+  const context = contextSelect.value;
+  const inputValue = inputEl.value;
+  const contextEl = document.createElement(context);
+  contextEl.setHTML(inputValue, {});
+  toutEl.value = contextEl.innerHTML;
+  houtEl.replaceChildren(contextEl);
 }
 
-inputEl.onkeyup = () => {};
